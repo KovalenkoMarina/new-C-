270c9a03323dd34efc64f848960a1960b370ff2b
Marina <kovalenko_marina@inbox.ru>
2022-05-08 15:39:59 +0700

    функция по поиску мах числа

---
+++ ex8/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 10;
+int b1 = 20;
+int c1 = 6;
+int a2 = 8;
+int b2 = 4;
+int c2 = 1;
+int a3 = 2;
+int b3 = 60;
+int c3 = 89;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+Console.Write("max = ");
+Console.WriteLine(max);
