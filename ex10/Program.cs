46ef481fb2e29a7237cf8840472e3e6de176087a
Marina <kovalenko_marina@inbox.ru>
2022-05-09 10:54:47 +0700

    поиск номера члена массива с определенным значением, если таких элементов несколько перебор останавливается при определении первого

---
+++ ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 43, 72, 82, 3, 211, 93, 211 };
+
+int n = array.Length;
+int find = 211;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
